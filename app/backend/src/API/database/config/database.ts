447d28195280187364083dee71ead986eb1eaa82
JoaoPedroLage <joaopedro.lage@outlook.com>
2022-07-22 00:20:40 -0300

    fix: install and correct dependencies

--- app/backend/src/API/database/config/database.ts
+++ app/backend/src/API/database/config/database.ts
@@ -2,11 +2,11 @@ import 'dotenv/config';
 import { Options } from 'sequelize';
 
 const config: Options = {
-  username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '76159900',
-  database: process.env.DB_NAME || 'TASKS_MANAGER',
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 3306,
+  username: 'root',
+  password: 'mysql098',
+  database: 'TASKS_MANAGER',
+  host: 'localhost',
+  port: 3306,
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
